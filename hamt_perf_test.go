1c94213a16769eeac679e0122f6c851c66c8137c
Jim Dixon <jddixon@gmail.com>
2014-04-20 20:10:23 -0700

    added w=4,5,6,7,8 HAMT perf tests

--- hamt_perf_test.go
+++ hamt_perf_test.go
@@ -40,11 +40,27 @@ func makeSomeMoreKeys(N, K int) (rawKeys [][]byte, bKeys []*BytesKey) {
 
 // -- tests proper --------------------------------------------------
 
-func (s *XLSuite) BenchmarkMakeSomeMoreKeys(c *C) {
-	if VERBOSITY > 0 {
-		fmt.Println("BENCHMARK: HAMT MAKE_SOME_MORE_KEYS")
-	}
-
+func (s *XLSuite) BenchmarkHAMT_4(c *C) {
+	if VERBOSITY > 0 { fmt.Println("\nBenchmarkHAMT_4") }
+	s.doBenchmark(c, uint(4), 0)
+}
+func (s *XLSuite) BenchmarkHAMT_5(c *C) {
+	if VERBOSITY > 0 { fmt.Println("\nBenchmarkHAMT_5") }
+	s.doBenchmark(c, uint(5), 0)
+}
+func (s *XLSuite) BenchmarkHAMT_6(c *C) {
+	if VERBOSITY > 0 { fmt.Println("\nBenchmarkHAMT_6") }
+	s.doBenchmark(c, uint(6), 0)
+}
+func (s *XLSuite) BenchmarkHAMT_7(c *C) {
+	if VERBOSITY > 0 { fmt.Println("\nBenchmarkHAMT_7") }
+	s.doBenchmark(c, uint(7), 0)
+}
+func (s *XLSuite) BenchmarkHAMT_8(c *C) {
+	if VERBOSITY > 0 { fmt.Println("\nBenchmarkHAMT_8") }
+	s.doBenchmark(c, uint(8), 0)
+}
+func (s *XLSuite) doBenchmark(c *C, w, t uint) {
 	// build an array of N random-ish K-byte rawKeys
 	K := 16
 	N := c.N
@@ -54,13 +70,10 @@ func (s *XLSuite) BenchmarkMakeSomeMoreKeys(c *C) {
 	deltaT := t1.Sub(t0)
 	fmt.Printf("setup time for %d %d-byte rawKeys: %v\n", N, K, deltaT)
 
-	w := uint(5)
-	t := uint(0)
 	m := NewHAMT(w, t)
 
 	c.ResetTimer()
 	c.StartTimer()
-	// my results: 1603 ns/op for a run of 1 million insertions
 	for i := 0; i < c.N; i++ {
 		_ = m.Insert(bKeys[i], &rawKeys[i])
 	}
@@ -79,6 +92,11 @@ func (s *XLSuite) BenchmarkMakeSomeMoreKeys(c *C) {
 		c.Assert(bytes.Equal(*val, rawKeys[i]), Equals, true)
 
 	}
-	// A BIT OF A HACK
-	fmt.Printf("table count = %d\n", m.root.GetTableCount())
+	// rough measure of resource consumption
+	tableCount := m.root.GetTableCount()
+	slotCount := m.root.maxSlots * tableCount
+	byteCount := slotCount * 8
+	megabytes := float32(byteCount) / (1024*1024)
+	fmt.Printf("%6d tables, %7d slots, %3.1f megabytes\n", 
+		tableCount, slotCount, megabytes)
 }
